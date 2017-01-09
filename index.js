37b1114317574b1588263950c3e6cddec4e9f0e9
jens-o <Jens@iMac.local>
2017-01-09 19:42:30 +0100

    temp

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ RpiTemp.prototype = {
                     } else {
                         //Divide the response by 1000 to get degrees celsius
                         that.temperature = parseFloat(stdout)/1000;
-                        that.log('Sending temp: ' + that.temperature);
                     }
                 });
                 break;
